863847424edd24750c74fb9601ced4ac3f1028dc
Johann Reinke <johann.reinke@gmail.com>
2012-04-28 10:39:23 +0200

    Allowed recursive directory scan of test classes

--- shell/mink.php
+++ shell/mink.php
@@ -6,9 +6,9 @@ class Mage_Shell_Mink extends Mage_Shell_Abstract
 {
     public function run()
     {
-        $pattern = array('app', 'code', 'local', '*', '*', 'Test', 'Mink', '*.php');
+        $pattern = array('app', 'code', 'local', '*', '*', 'Test', 'Mink');
         $pattern = Mage::getBaseDir() . DS . implode(DS, $pattern);
-        $files = @glob($pattern);
+        $files = $this->_getTestClasses($pattern);
         $renderer = $this->_getOutputRenderer();
         if (empty($files)) {
             $renderer->output($renderer->bold('No test class found'));
@@ -22,8 +22,16 @@ class Mage_Shell_Mink extends Mage_Shell_Abstract
                 $start = strrpos($file, 'local');
                 $classPath = substr($file, $start + 6);
                 $className = basename(implode('_', explode(DS, $classPath)), '.php');
+                if (!class_exists($className)) {
+                    $renderer->error(sprintf('Class %s does not exist', $className));
+                    continue;
+                }
                 $object = new $className($driver, $renderer);
                 $reflection = new ReflectionClass($className);
+                if (!$reflection->getParentClass() || $reflection->getParentClass()->getName() !== 'JR_Mink_Test_Mink') {
+                    $renderer->error(sprintf('Class %s must extend JR_Mink_Test_Mink class', $className));
+                    continue;
+                }
                 $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
                 foreach ($methods as $method) {
                     if (substr($method->getName(), 0, 4) !== 'test') {
@@ -68,6 +76,24 @@ class Mage_Shell_Mink extends Mage_Shell_Abstract
         }
 
         return $driver;
+    }
+
+    protected function _getTestClasses($dir)
+    {
+        $files = array();
+        $items = glob($dir . '*', GLOB_MARK | GLOB_NOSORT);
+
+        foreach ($items as $item) {
+            if (substr($item, -1) != DS) {
+                if (pathinfo($item, PATHINFO_EXTENSION) === 'php') {
+                    $files[] = $item;
+                }
+            } else {
+                $files = array_merge($files, $this->_getTestClasses($item));
+            }
+        }
+
+        return $files;
     }
 
     protected function _validate()
